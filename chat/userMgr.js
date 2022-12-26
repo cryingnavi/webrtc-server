515b4e62b95e93147d004e66a08f6f439e0863d8
cryingnavi <cryingnavi@gmail.com>
2022-12-26 10:02:06 +0900

    Create userMgr.js

---
+++ chat/userMgr.js
@@ -0,0 +1,12 @@
+var User = require('./user');
+
+module.exports = {
+  users: {},
+  getUser: function (userId) {
+    return this.users[userId] || null;
+  },
+  createUser: function (userId, ws) {
+    var user = this.users[userId] = new User(userId, ws);
+    return user;
+  }
+};
